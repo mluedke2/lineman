bcf3fbb59e50a222d3c02f5ba0a0d01ca09340c5
Matt Luedke <mluedke2@gmail.com>
2013-06-19 16:34:10 -0700

    Initial Commit

---
+++ TrackpadTest/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TrackpadTest
+//
+//  Created by Matt Luedke on 6/19/13.
+//  Copyright (c) 2013 Matt Luedke. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
---
+++ TrackpadTestTests/TrackpadTestTests.m
@@ -0,0 +1,36 @@
+//
+//  TrackpadTestTests.m
+//  TrackpadTestTests
+//
+//  Created by Matt Luedke on 6/19/13.
+//  Copyright (c) 2013 Matt Luedke. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TrackpadTestTests : XCTestCase
+
+@end
+
+@implementation TrackpadTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
